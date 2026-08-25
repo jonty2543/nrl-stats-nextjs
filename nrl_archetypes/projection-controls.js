8b2ab033d54617b7a84027cebc3813e0f1a45566
Jonty <jontyandrew2003@gmail.com>
2026-08-25 23:30:51 +1000

    Revert "Add border to archetype plot area"
    This reverts commit 044c5f1005a639b162927d9dd4b702fc375d6160.

--- nrl_archetypes/projection-controls.js
+++ nrl_archetypes/projection-controls.js
@@ -519,8 +519,6 @@
       #plotly-wrapper {
         position: relative;
         min-height: 0;
-        box-sizing: border-box;
-        border: 1px solid rgba(0, 245, 138, 0.55);
       }
       #plotly-wrapper .plotly-graph-div {
         height: 100% !important;
